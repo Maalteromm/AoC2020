ed8878428e263eae726c1c8a1a183b7a250698f4
Maalteromm <burlerosado@yahoo.com.br>
2020-12-15 19:52:14 -0300

    day 14

--- day_14/day_14.py
+++ day_14/day_14.py
@@ -2,8 +2,10 @@ class docking_aid:
   '''Help the ship dock by solving the docking parameters.'''
   def __init__(self, filename='input'):
     self.filename = filename
-    self.test()
-    self.day_14()
+    self.test('p1')
+    self.day_14('p1')
+    self.test('p2')
+    self.day_14('p2')
     return
 
   def load_file(self):
@@ -11,7 +13,7 @@ class docking_aid:
       data = [line.strip() for line in file]
     return data
 
-  def docking_program(self, data):
+  def docking_program(self, data, part):
     mask = ''
     mem = {}
     for line in data:
@@ -20,7 +22,12 @@ class docking_aid:
         mask = scd_half
       elif fst_half.startswith('mem'):
         start, stop = fst_half.find('[') + 1, fst_half.find(']')
-        mem[fst_half[start:stop]] = self.apply_bitmask(scd_half, mask)
+        if part == 'p1':
+          mem[fst_half[start:stop]] = self.apply_bitmask(scd_half, mask)
+        elif part == 'p2':
+          address = fst_half[start:stop]
+          addresses = self.apply_bitmask_v2(address, mask)
+          for address in addresses: mem[address] = int(scd_half)
     result = 0
     for value in mem.values():
       result += value
@@ -34,21 +41,56 @@ class docking_aid:
                      for val, m_val in zip(value, mask)])
     return int(value, base=2)
 
+  def apply_bitmask_v2(self, value, mask):
+    value = bin(int(value))[2:]
+    for zero in range(len(mask) - len(value)):
+      value = '0' + value
+    value = ''.join(['X' if m_val == 'X' else '1' if m_val == '1' else val
+                     for val, m_val in zip(value, mask)])
+    addresses = self.address_combinations(value)
+    return [int(address, base=2) for address in addresses]
 
-  def test(self):
-    sample = '''mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+  def address_combinations(self, value, addresses=[]):
+    addresses = []
+    addresses.append(value)
+    while True:
+      tmp_addrss = []
+      addresses_len = 0
+      for address in addresses:
+        if 'X' in address:
+          tmp_addrss.append(address.replace('X', '0', 1))
+          tmp_addrss.append(address.replace('X', '1', 1))
+        else:
+          addresses_len += 1
+      if addresses_len == len(addresses):
+        break
+      addresses = tmp_addrss.copy()
+    return addresses
+        
+
+
+  def test(self, part):
+    if part == 'p1':
+      sample = '''mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0'''
+    elif part == 'p2':
+      sample = '''mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1'''
     sample = sample.split('\n')
-    test = self.docking_program(sample)
-    if test == 165:
-      print('Test {} successful.'.format('p1'))
+    test = self.docking_program(sample, part)
+    if test == 165 and part == 'p1':
+      print('Test {} successful.'.format(part))
+    elif test == 208 and part == 'p2':
+      print('Test {} successful.'.format(part))
     return
 
-  def day_14(self):
-    answer = self.docking_program(self.load_file())
-    print('Answer: {}'.format(answer))
+  def day_14(self, part):
+    answer = self.docking_program(self.load_file(), part)
+    print('Answer {}: {}'.format(part, answer))
     return
 
 docking_aid()
